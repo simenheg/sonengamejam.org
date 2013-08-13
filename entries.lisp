6c893885b156c7e7510e08bf86c60c90e5c553f1
Simen Heggestøyl <simenheg@ifi.uio.no>
2013-08-13 15:48:18 +0200

    Initial commit

---
+++ entries.lisp
@@ -0,0 +1,145 @@
+;; Local Variables:
+;; eval: (put 'defentry 'lisp-indent-function 1)
+;; End:
+
+;; ----------------------------------------------------------- [ Frameworks ]
+
+(defstruct (framework (:constructor make-framework (name written url)))
+  name written url)
+
+(defparameter *known-frameworks*
+  (mapcar
+   (lambda (fw) (apply #'make-framework fw))
+   '((processing "Processing" "http://processing.org/")
+     (gamemaker "GameMaker" "http://www.yoyogames.com/")
+     (love "LÖVE" "http://love2d.org/")
+     (unity "Unity" "http://unity3d.com/")
+     (cocos2d "cocos2d" "http://cocos2d.org/")
+     (amos "AMOS" "https://en.wikipedia.org/wiki/AMOS_%28programming_language%29")
+     (melonjs "melonJS" "http://melonjs.org/"))))
+
+;; ----------------------------------------------------- [ Download formats ]
+
+(defun format-image (format)
+  (concatenate
+   'string "icons/"
+   (ecase format
+     (:bitbucket "bitbucket.png")
+     (:github "github.png")
+     (:zip "zip.png"))))
+
+;; -------------------------------------------------------------- [ Entries ]
+
+(defstruct entry
+  (team-name "anonymous" :type string)
+  (game-title "n/a" :type string)
+  (team-members '() :type list)
+  (framework nil)
+  (downloads '() :type list) ; a plist in form (:format url)
+  (rank nil)
+  (screenshot nil))
+
+(defparameter *entries* (make-hash-table :test #'equal))
+
+(defun defentry (year &rest args)
+  (let ((new-entry (apply #'make-entry args)))
+    (setf (entry-team-members new-entry) ; keep team member list sorted
+          (sort (entry-team-members new-entry) #'string<))
+    
+    (when-let ((framework-name (entry-framework new-entry)))
+      (let ((framework (find framework-name *known-frameworks*
+                             :key #'framework-name)))
+        (setf (entry-framework new-entry)
+              (or framework (make-framework nil "n/a" "#")))))
+    
+    (pushnew new-entry (gethash year *entries*)
+             :key #'entry-team-name     ; only one entry per team
+             :test #'string=)))
+
+;; ------------------------------------------------------- [ Define entries ]
+
+(defentry "May 2013"
+  :team-name "CYBRILL"
+  :game-title "The Flood"
+  :team-members '("Thor Merlin Lervik")
+  :framework 'processing
+  :downloads '(:zip "http://sonen.ifi.uio.no/w/images/b/b2/game-jam-2013-escape.zip")
+  :screenshot "the-flood-2.png")
+
+(defentry "May 2013"
+  :team-name "Fat Pixie"
+  :game-title "Bob in Chains"
+  :team-members '("Kevin Engelsjord"
+                  "Peter Rasmussen Lubiana"
+                  "Robin Aasen")
+  :framework 'gamemaker
+  :downloads '(:zip "http://sonen.ifi.uio.no/w/images/7/7f/fat_pixie_studios-bob-in-chains-0.1.zip")
+  :screenshot "bob-in-chains.png")
+
+(defentry "May 2013"
+  :team-name "Game Jam organizers"
+  :game-title "Pig Run"
+  :team-members '("Simen Heggestøyl")
+  :framework 'love
+  :downloads '(:zip "http://folk.uio.no/simenheg/pig-run.love")
+  :screenshot "pig-run.png")
+
+(defentry "May 2013"
+  :team-name "keep it lol"
+  :game-title "Fishly"
+  :team-members '("Lorenz Kellerer")
+  :framework 'unity
+  :downloads '(:zip "http://sonen.ifi.uio.no/w/images/c/cb/code_keep_it_lol.zip"))
+
+(defentry "May 2013"
+  :team-name "Níðhǫggr"
+  :team-members '("Aleksi Miikkael Luukkonen"
+                  "Aron Jansson Nordberg"
+                  "Bjørn-Ingar Bergum"
+                  "Gorm Lundestad"
+                  "Lars Bjørlykke Kristiansen"
+                  "Stine Skillebek")
+  :framework 'cocos2d
+  :downloads '(:github "https://github.com/LarsBK/sonengamejam/"))
+
+(defentry "May 2013"
+  :team-name "No Name, No Game"
+  :game-title "The aMAZEing Escape"
+  :team-members '("Daniel Rødskog" "Erlend Kristiansen")
+  :framework 'processing
+  :downloads '(:bitbucket "https://bitbucket.org/HrKristiansen/gamejam2013")
+  :screenshot "the-amazeing-escape.png")
+
+(defentry "May 2013"
+  :team-name "Rancid Applications"
+  :game-title "Rancid"
+  :team-members '("Martin Stensgård")
+  :framework 'amos
+  :downloads '(:zip "http://sonen.ifi.uio.no/w/images/0/0a/Rancid.zip"))
+
+(defentry "May 2013"
+  :team-name "Team Beam"
+  :game-title "Dark Escape"
+  :team-members '("Julian Hisdal Nymark"
+                  "Kyrre Havik Eriksen")
+  :framework 'love
+  :downloads '(:zip "http://sonen.ifi.uio.no/w/images/2/2f/team-beam-dark-escape.zip"
+               :bitbucket "https://bitbucket.org/beamteam/gamejamgame")
+  :screenshot "dark-escape.png")
+
+(defentry "May 2013"
+  :team-name "Team Duckhunt"
+  :game-title "The Escape"
+  :team-members '("Andreas Orvik" "Ivar Tryti" "Stian Valle")
+  :framework 'love
+  :downloads '(:zip "http://sonen.ifi.uio.no/w/images/0/03/TheEscape.zip")
+  :screenshot "the-escape.png")
+
+(defentry "May 2013"
+  :team-name "Team Kyrre"
+  :team-members '("Asbjørn Lysne Voje"
+                  "Halvard Eggen"
+                  "Linda Bech"
+                  "Tommy Vitikka")
+  :framework 'melonjs
+  :downloads '(:zip "http://sonen.ifi.uio.no/w/images/c/c9/Team_kyrre.zip"))
---
+++ gamejam.lisp
@@ -0,0 +1,298 @@
+;; Hunchentoot examples are found at:
+;;  ~/quicklisp/dists/quicklisp/software/hunchentoot-1.2.3/test
+;;
+;; Launch hunchentoot tests:
+;;  (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 8080))
+;;
+;; Hunchentoot test URI:
+;;  http://127.0.0.1:8080/hunchentoot/test
+
+(ql:quickload :hunchentoot)
+(ql:quickload :hunchentoot-test)
+(ql:quickload :cl-who)
+(ql:quickload :ht-simple-ajax)
+(ql:quickload :cl-json)
+(ql:quickload :alexandria)
+
+(defpackage :gamejam
+  (:use :cl :hunchentoot :cl-who :ht-simple-ajax :json :alexandria)
+  (:export :start-server))
+
+(in-package :gamejam)
+
+(load "entries.lisp")
+
+;; ---------------------------------------------------------------- [ Misc. ]
+;; *ENGLISH-LIST* format directive:
+;;   (format nil *english-list* '(1))       ==> "1"
+;;   (format nil *english-list* '(1 2))     ==> "1 and 2"
+;;   (format nil *english-list* '(1 2 3))   ==> "1, 2, and 3"
+
+(defparameter *english-list*
+  "~{~#[~;~a~;~a and ~a~:;~@{~a~#[~;, and ~:;, ~]~}~]~}")
+
+;; ---------------------------------------------------------------- [ Notes ]
+(defclass note ()
+  ((text :initarg :text)
+   (x :initarg :x)
+   (y :initarg :y)))
+
+(defparameter *notes* '())
+
+;; ----------------------------------------------------------------- [ AJAX ]
+(defparameter *ajax-processor* 
+  (make-instance 'ajax-processor :server-uri "/ajax")) 
+
+(defun-ajax post-note (text x y) (*ajax-processor*)
+  (push (make-instance 'note :text text :x x :y y) *notes*)
+  "ok")
+
+(defun-ajax get-notes () (*ajax-processor*)
+  (encode-json-to-string *notes*))
+
+;; ------------------------------------------------------ [ Server settings ]
+(setq
+ *dispatch-table*
+ (list
+  (create-ajax-dispatcher *ajax-processor*)
+  (create-static-file-dispatcher-and-handler
+   "/style.css" (first (directory "css/style.css")))
+  (create-static-file-dispatcher-and-handler
+   "/js.js" (first (directory "js/js.js")))
+  (create-folder-dispatcher-and-handler
+   "/icons/" (first (directory "icons/")))
+  (create-folder-dispatcher-and-handler
+   "/screenshots/" (first (directory "screenshots/")))
+  (create-prefix-dispatcher "/matchmaking" 'matchmaking)
+  (create-prefix-dispatcher "/entries" 'entries)
+  (create-prefix-dispatcher nil 'index)))
+
+;; ---------------------------------------------------------- [ HTML macros ]
+(defmacro with-html-page (&body body)
+  `(with-html-output-to-string
+       (*standard-output* nil :prologue t :indent nil)
+     ,@body))
+
+(defmacro with-html (&body body)
+  `(with-html-output-to-string
+       (*standard-output* nil :indent nil)
+     ,@body))
+
+;; ------------------------------------------------------ [ HTML generation ]
+(defparameter +index-color+ "#a7e1ed")
+(defparameter +matchmaking-color+ "#daef80")
+(defparameter +entries-color+ "#f49896")
+
+(defun html-render-header ()
+  (with-html
+    (:div
+     :id "header"
+     "( maybe header. or maybe not )")))
+
+(defun html-render-menu-item (name ref)
+  (with-html
+    (:div
+     :class "menu-item-container"
+     (:a
+      :href ref
+      (:div
+       :class "menu-item"
+       :id (concatenate 'string "menu-item-" ref)
+       (fmt name))))))
+
+(defun html-render-menu ()
+  (with-html
+    (:div
+     :id "menu"
+     (fmt (html-render-menu-item "Info" "index"))
+     (fmt (html-render-menu-item "Matchmaking" "matchmaking"))
+     (fmt (html-render-menu-item "Entries" "entries")))))
+
+(defun html-render-body-index ()
+  (with-html
+    (:div
+     :id "body"
+     :style (format nil "background-color: ~a;" +index-color+)
+     (:p
+      "Make a game in 48 hours! The purpose of the Game Jam is to gather
+       aspiring game developers, rookies and veterans alike. Everyone is
+       eligible enter, and the entry is no fee. No prior registration is
+       needed, all you have to do is to show up at Sonen, Ole-Johan Dahls
+       hus, September 27th!")
+     (:h3 "Rules")
+     (:p (:i "Note, all rules are subject to change before September 27th!"))
+     (:ul
+      (:li "One game submission per team.")
+      (:li "There is no limit on the number of team members.")
+      (:li "Time limit: 48 hours.")
+      (:li "Game must conform with the given theme.")
+      (:li "All programming languages allowed.")
+      (:li "All publicly available frameworks, libraries & assets allowed.")
+      (:li "Source code must be included in the final delivery."))
+     (:h3 "Timetable")
+     (:table
+      (:tr (:td :colspan "2" (:strong "Friday 27th")))
+      (:tr (:td "15:30") (:td "Registrations open"))
+      (:tr (:td "16:00") (:td "Crashcourse in LÖVE (Lua)"))
+      (:tr (:td "16:30") (:td "Crashcourse in Processing (Java)"))
+      (:tr (:td "17:00") (:td "Theme announcement & starting shot"))
+      (:tr (:td "17:15") (:td "Crashcourse in game physics MOVED FROM
+                               SATURDAY"))
+      (:tr (:td "24:00") (:td "Time to go home "))
+
+      (:tr (:td :colspan "2" (:strong "Saturday 28th")))
+      (:tr (:td "10:00") (:td "Doors open"))
+      (:tr (:td "18:00") (:td "Crashcourse in pixelart"))
+      (:tr (:td "19:00") (:td "Crashcourse in game music"))
+      (:tr (:td "24:00") (:td "Time to go home "))
+
+      (:tr (:td :colspan "2" (:strong "Sunday 29th")))
+      (:tr (:td "10:00") (:td "Doors open"))
+      (:tr (:td "17:00") (:td "Submission deadline"))
+      (:tr (:td "17:15") (:td "Showoff on the big screen"))
+      (:tr (:td "Afterwards") (:td "Prizes & pizza "))
+      )
+     (:h3 "Matchmaking")
+     (:p
+      "Still missing that special someone on your team that can make you
+       feel all warm and fuzzy inside (or at least turn your pixel-poop into
+       something you might dare to call \"art\" when nobody is listening?)")
+     (:i "Matchmaking service to come!")
+     (:h3 "Not associated with UiO?")
+     (:p
+      "Guest accounts will be provided, allowing you to use the machines at
+       Ifi during the event.")
+     (:p "When you need access to the building, please call one of us:")
+     (:ul
+      (:li "Simen (96 82 24 48)")
+      (:li "Ilyá (96 82 24 48)"))
+     (:h3 "Pizza, coffee & other foods")
+     (:p
+      "Free pizza will be served Sunday afternoon. Please inform us
+       beforehand if you've got any specific dietary needs. ")
+     (:p "Free coffee will of course be available at all times."))))
+
+(defun html-render-body-matchmaking ()
+  (with-html
+    (:div
+     :id "body"
+     :style (format nil "background-color: ~a;" +matchmaking-color+)
+     (:i "( matchmaking service in the future )")
+     (:p
+      (:canvas
+       :id "matchmaking-canvas"
+       :width 540
+       :height 540
+       :style "border:1px solid black; cursor: none;")))))
+
+(defun html-render-download-links (download-plist)
+  (with-html
+    (doplist (format url download-plist)
+      (htm (:a :href url (:img :src (format-image format))))
+      (fmt " "))))
+
+(defun html-render-entry (entry)
+  (with-html
+    (:div
+     :class "entry"
+     
+     ;; Title
+     (:h2 (fmt (entry-game-title entry)))
+     
+     ;; Screenshot
+     (when-let* ((screenshot (entry-screenshot entry))
+                 (uri (concatenate 'string "screenshots/" screenshot)))
+       (htm (:div :class "screenshot" (:a :href uri (:img :src uri)))))
+
+     ;; Attribution
+     (:p
+      (fmt "By ~a " (with-html (:strong (fmt (entry-team-name entry)))))
+      (:br)
+      (:span :style "font-size: 0.75em;"
+             (fmt (concatenate 'string "(" *english-list* ")")
+                  (entry-team-members entry))))
+
+     ;; Framework
+     (:p
+      (fmt "Framework: ~a"
+           (let ((framework (entry-framework entry)))
+             (with-html
+               (:a :href (framework-url framework)
+                   (fmt (framework-written framework)))))))
+
+     ;; Download links
+     (:p (fmt (html-render-download-links (entry-downloads entry)))))))
+
+(defun html-render-entries (gamejam)
+  (with-html
+    (:h1 (fmt gamejam))
+    (let ((entries ; the entry list, sorted on game title
+           (sort (copy-list (gethash gamejam *entries*))
+                 #'string< :key #'entry-game-title)))
+      (dolist (entry entries)
+        (fmt (html-render-entry entry))))))
+
+(defun html-render-body-entries ()
+  (with-html
+    (:div
+     :id "body"
+     :style (format nil "background-color: ~a;" +entries-color+)
+     (fmt (html-render-entries "May 2013")))))
+
+(defun html-render-footer ()
+  (with-html
+    (:div
+     :id "footer"
+     (fmt "Contact: ~a"
+          (with-html (:a :href "mailto:simenheg@ifi.uio.no"
+                         "Simen Heggestøyl"))))))
+
+(defun html-render-site (site)
+  (with-html-page
+    (:head (:meta
+            :charset "utf-8")
+           (princ
+            (generate-prologue *ajax-processor*))
+           (:script
+            :type "text/javascript"
+            :src "http://code.jquery.com/jquery-latest.min.js")
+           (:script
+            :src "js.js")
+           (:link
+            :rel "stylesheet"
+            :type "text/css"
+            :href "style.css")
+           (:title "Game Jam 2013"))
+
+    (:body
+     :onload "initCanvas()"
+     (:div
+      :id "frame"
+      (fmt (html-render-header))
+      (fmt (html-render-menu))
+      (fmt (ecase site
+             (index (html-render-body-index))
+             (matchmaking (html-render-body-matchmaking))
+             (entries (html-render-body-entries))))
+      (fmt (html-render-footer))))))
+
+(define-easy-handler (index) ()
+  (no-cache)
+  (html-render-site 'index))
+
+(define-easy-handler (matchmaking) ()
+  (no-cache)
+  (html-render-site 'matchmaking))
+
+(define-easy-handler (entries) ()
+  (no-cache)
+  (html-render-site 'entries))
+
+;; ----------------------------------------------------------------- [ Run! ]
+
+(defun start-server (&key (port 8080))
+  (start (make-instance 'easy-acceptor :port port)))
+
+(handler-case (start-server :port 8080)
+  (usocket:address-in-use-error ()
+    (format t "No server started; address in use.~%")))
