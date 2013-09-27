7bb808c7b590389e462ddf1ed4179ca3bc3e5d36
Simen Heggestøyl <simenheg@ifi.uio.no>
2013-09-27 14:14:11 +0200

    Changes to the schedule

--- timetable.lisp
+++ timetable.lisp
@@ -1,12 +1,11 @@
 (("Friday 27th"
   ("15:00" "Registrations open & matchmaking")
   ("15:30" "Talk: Crashcourse in LÖVE (Lua)")
-  ("16:30" "Talk: Crashcourse in Processing (Java)")
   ("17:00" "Theme announcement & starting shot")
+  ("17:05" "Talk: Crashcourse in Processing (Java)")
   ("24:00" "Time to go home "))
  ("Saturday 28th"
   ("10:00" "Doors open")
-  ("12:00" "Talk: Crashcourse in game physics (maybe)")
   ("18:00" "Talk: Crashcourse in pixelart")
   ("21:00" "Talk: Anatomy of an arcade machine")
   ("24:00" "Time to go home "))
@@ -14,4 +13,4 @@
   ("10:00" "Doors open")
   ("17:00" "Submission deadline")
   ("17:15" "Showoff on the big screen")
-  ("Afterwards" "Prizes & pizza ")))
+  ("Afterwards" "Prizes & pizza")))
